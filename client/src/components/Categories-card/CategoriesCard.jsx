1a73912c13e372e11c136e23c18ddd5b83fed752
Sergey Shpak <sergey.shpak79@gmail.com>
2024-07-12 01:10:53 +0300

    change vercel.json

--- client/src/components/Categories-card/CategoriesCard.jsx
+++ client/src/components/Categories-card/CategoriesCard.jsx
@@ -12,7 +12,7 @@ function CategoriesCard() {
               <img src="/img/sourdough_bread_3.jpg" />
               <article>
                 <p className="text-orange-500 text-2xl font-bold pt-5">Bread</p>
-                <p className="text-gray-500 hidden lg:block text-xl pt-5">
+                <p className="text-gray-500 hidden lg:block text-lg pt-5">
                   We take pride in offering 
                   bread that is both delicious and healthy. Our bakery adheres
                   to the finest traditions, ensuring each loaf is crafted with
@@ -31,7 +31,7 @@ function CategoriesCard() {
               <img src="/img/croissant_3.jpg" />
               <article>
                 <p className="text-orange-500 text-2xl font-bold pt-5">Bakery</p>
-                <p className="text-gray-500 hidden lg:block text-xl pt-5">
+                <p className="text-gray-500 hidden lg:block text-lg pt-5">
                 Indulge in our delectable baked goods, crafted with love and care. From flaky croissants to heavenly pastries, each bite is a delightful journey of flavor. Our bakery follows time-honored traditions, ensuring every treat is both delicious and healthy. Explore our selection and discover the perfect pastry to sweeten your day!
                 </p>
               </article>
@@ -42,7 +42,7 @@ function CategoriesCard() {
               <img src="/img/vanilla_cupcake_3.jpg" />
               <article>
                 <p className="text-orange-500 text-2xl font-bold pt-5"> Cakes</p>
-                <p className="text-gray-500 hidden lg:block text-xl pt-5">
+                <p className="text-gray-500 hidden lg:block text-lg pt-5">
                 Delight in our exquisite desserts, meticulously crafted to satisfy your sweetest cravings. From rich, creamy cakes to delicate tarts, each dessert is a masterpiece of flavor and texture. Our bakery takes pride in using the finest ingredients, ensuring every bite is a moment of pure indulgence. Treat yourself to our selection of desserts and elevate your taste experience to new heights!
                 </p>
               </article>
