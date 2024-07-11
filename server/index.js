768b1f8e4ec6914fb0e08cf4d2fdcd341ad14cc0
Sergey Shpak <sergey.shpak79@gmail.com>
2024-07-12 01:24:02 +0300

    ttps://vercel.com/sergio-916s-projects/ecommerce-bakery

--- server/index.js
+++ server/index.js
@@ -35,13 +35,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-bakery.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-bakery.vercel.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 
 const appstart = async () => {
